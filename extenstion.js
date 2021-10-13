f80a3c3b4dd0fe406ae436b664a5b94125523fba
Lizzie <80986952+Lizzienduka@users.noreply.github.com>
2021-10-13 15:39:50 +0100

    sales lead tracker

---
+++ extenstion.js
@@ -0,0 +1,69 @@
+let myLeads = []
+let oldleads = []
+
+
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
+
+
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+tabBtn.addEventListener("click", function() {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+            myLeads.push(tabs[0].url)
+            localStorage.setItem("myLead", JSON.stringify(myLeads) )
+            render(myLeads)
+        })
+    
+});
+
+
+
+
+deleteBtn.addEventListener('dblclick', function() {
+    console.log("cliked twice")
+    localStorage.clear();
+    myLeads = []
+    render(myLeads)
+    
+
+
+});
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+
+}); 
+
+
+
+function render(leads) {
+    let listItems = ""
+for (i = 0; i < leads.length; i++) {
+    ulEl.innerHTML += "<li>" + leads[i] + " </li>" 
+ 
+
+    listItems += `
+    <li>
+        <a href='${leads[i]}' target='_blank'> ${leads[i]}  
+        </a> 
+    </li>`
+}
+
+ulEl.innerHTML = listItems
+}
+
+// console.log( localStorage.getItem("myNewLead"))
+// localStorage.clear()
